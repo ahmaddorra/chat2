78144ee2e9ae00fe50ee804c198be479f96688f1
ahmaddorra <ahmadeldorra1@gmail.com>
2021-03-17 14:38:22 +0200

    auth

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -19,3 +20,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 */
+Route::get("/", [UserController::class,'index']);
